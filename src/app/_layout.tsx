067f9efbbfe374ba5e6a0734739a7571c530e8c7
youichi Ishigaki <deepestred1968@gmail.com>
2024-03-02 17:17:13 +0900

    expo router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Slot } from "expo-router"
+import { View } from "react-native";
+
+const Layout = () => {
+  return (
+    <View>
+        <Slot/>
+    </View>
+    
+    )
+}
+
+export default Layout;
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native"
+
+const Index = () => {
+    return (
+        <View>
+            <Text style={{color:"#000"}}> Hello World!</Text>
+        </View>
+    )
+}
+
+export default Index;
