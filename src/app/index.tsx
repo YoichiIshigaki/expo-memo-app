9f40c3c8cf00926577855faf11fc3e0a1c45e546
youichi Ishigaki <deepestred1968@gmail.com>
2024-03-03 12:42:37 +0900

    styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,42 +3,27 @@ import { View, Text, StyleSheet } from 'react-native';
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Hello World!</Text>
-          <Text>Logout</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
-      {/* todo 1つ目 */}
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>{new Date().toISOString()}</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-      {/* todo 2つ目 */}
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>{new Date().toISOString()}</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-      {/* todo 3つ目 */}
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>{new Date().toISOString()}</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
+      {['買い物', '筋トレ', '瞑想'].map((v) => {
+        return (
+          <View style={styles.memoListItem}>
+            <View>
+              <Text style={styles.memoListItemTitle}>{v}</Text>
+              <Text style={styles.memoListItemDate}>
+                {new Date().toISOString()}
+              </Text>
+            </View>
+            <View>
+              <Text>X</Text>
+            </View>
+          </View>
+        );
+      })}
       {/*追加ボタン */}
       <View>
         <Text>+</Text>
@@ -50,8 +35,47 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
     alignItems: 'center',
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)',
+  },
+  memoListItem: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
   },
 });
 
